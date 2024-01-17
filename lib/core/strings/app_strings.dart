e343b2bfd108d0b27d13d4155587871f0044d83c
dev_muslim <jmappsmuslim@gmail.com>
2024-01-17 08:31:10 +0300

    Added styles

--- lib/core/strings/app_strings.dart
+++ lib/core/strings/app_strings.dart
@@ -1,3 +1,18 @@
 class AppStrings {
   static const String appName = 'Манба';
+  static const String searchWords = 'Поиск слов...';
+  static const String lastCollections = 'Последние коллекции';
+  static const String allCollection = 'Все коллекции';
+  static const String collectionsIfEmpty = 'Вы пока что не добавили ни одной коллекции';
+  static const String addCollection = 'Добавить коллекцию';
+  static const String collectionTitle = 'Название коллекции...';
+  static const String quiz = 'Викторина';
+  static const String wordsConstructor = 'Конструктор слов';
+  static const String cardMode = 'Режим карточек';
+  static const String add = 'Добавить';
+  static const String change = 'Изменить';
+  static const String deleteCollection = 'Удалить коллекцию';
+  static const String deleteAll = 'Удалить все коллекции';
+  static const String cancel = 'Отмена';
+  static const String selectColor = 'Выбор цвета';
 }
---
+++ lib/core/styles/app_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class AppStyles {
+  static const mainBorder = BorderRadius.all(Radius.circular(14));
+  static const mainBorderMini = BorderRadius.all(Radius.circular(7));
+  static const mainMarding = EdgeInsets.all(14);
+  static const mainMardingMini = EdgeInsets.all(7);
+  static const mardingWithoutBottom = EdgeInsets.fromLTRB(14, 14, 14, 0);
+  static const mardingWithoutTop = EdgeInsets.fromLTRB(14, 0, 14, 14);
+  static const mardingSymmetricHor = EdgeInsets.symmetric(horizontal: 14);
+  static const mardingSymmetricHorMini = EdgeInsets.symmetric(horizontal: 7);
+  static const mardingSymmetricVer = EdgeInsets.symmetric(vertical: 14);
+  static const mardingSymmetricVerMini = EdgeInsets.symmetric(vertical: 7);
+
+  static const collectionColors = <Color>[
+    CupertinoColors.systemBlue,
+    CupertinoColors.systemRed,
+    CupertinoColors.systemYellow,
+    CupertinoColors.systemGreen,
+    CupertinoColors.systemOrange,
+    CupertinoColors.systemTeal,
+    CupertinoColors.systemPurple,
+    CupertinoColors.systemIndigo,
+    CupertinoColors.systemFill,
+  ];
+}
