06355fdaa1a3e63ab176ac8a75fb552e52e158c5
dev_muslim <jmappsmuslim@gmail.com>
2024-01-17 08:32:02 +0300

    Added cupertino themes class

---
+++ lib/core/styles/cupertino_themes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class CupertinoThemes {
+  static CupertinoThemeData lightTheme = const CupertinoThemeData(
+    brightness: Brightness.light,
+  );
+  static CupertinoThemeData darkTheme = const CupertinoThemeData(
+    brightness: Brightness.dark,
+  );
+}
