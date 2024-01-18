fba043e0c11970aa8ff357f1e70769965c655c66
dev_muslim <jmappsmuslim@gmail.com>
2024-01-18 12:02:05 +0300

    Added main route names

--- lib/core/routes/route_names.dart
+++ lib/core/routes/route_names.dart
@@ -1,3 +1,6 @@
 class RouteNames {
   static const String allCollectionsPage = '/all_collections_page';
+  static const String quizPage = '/quiz_page';
+  static const String wordConstructorPage = '/word_constructor_page';
+  static const String cardModePage = '/card_mode_page';
 }
