c17b233ad7539568741a478141269a70d38362b3
dev_muslim <jmappsmuslim@gmail.com>
2024-01-21 18:55:53 +0300

    Added new order for main page widgets

--- lib/presentation/uiModules/ios/pages/main_cupertino_page.dart
+++ lib/presentation/uiModules/ios/pages/main_cupertino_page.dart
@@ -1,158 +1,113 @@
-import 'package:arabic/core/routes/route_names.dart';
-import 'package:arabic/core/strings/app_strings.dart';
-import 'package:arabic/core/styles/app_styles.dart';
-import 'package:arabic/presentation/uiModules/ios/pages/search_words_page.dart';
-import 'package:arabic/presentation/uiModules/ios/widgets/add_collection_dialog.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../../../../core/routes/route_names.dart';
+import '../../../../core/strings/app_strings.dart';
+import '../../../../core/styles/app_styles.dart';
+import '../items/main_card_item.dart';
 import '../lists/main_collections_list.dart';
+import '../widgets/add_collection_button.dart';
 import '../widgets/collections_order_button.dart';
+import '../widgets/main_search_word_text_field.dart';
 
 class MainCupertinoPage extends StatelessWidget {
   const MainCupertinoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text(AppStrings.appName),
-      ),
-      child: SafeArea(
-        right: false,
-        left: false,
-        child: Column(
-          children: [
-            Padding(
-              padding: AppStyles.mardingWithoutBottomMini,
-              child: CupertinoTextField(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(9),
-                  ),
-                  color: CupertinoColors.systemGrey5,
-                ),
-                readOnly: true,
-                prefix: const Padding(
-                  padding: AppStyles.mardingOnlyLeftMini,
-                  child: Icon(
-                    CupertinoIcons.search,
-                    color: CupertinoColors.systemGrey,
-                  ),
-                ),
-                placeholder: AppStrings.searchWords,
-                placeholderStyle: const TextStyle(
-                  fontSize: 16,
-                  color: CupertinoColors.systemGrey,
-                ),
-                onTap: () {
-                  showCupertinoModalPopup(
-                    context: context,
-                    builder: (_) => const SearchWordsPage(),
-                  );
-                },
+    return const CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemGroupedBackground,
+      child: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(
+            stretch: true,
+            middle: Text(
+              AppStrings.appName,
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'SF Pro',
               ),
             ),
-            const Row(
+            largeTitle: Padding(
+              padding: AppStyles.mardingOnlyRight,
+              child: MainSearchWordTextField(),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                SizedBox(width: 7),
-                Expanded(
-                  child: Text(
-                    AppStrings.lastCollections,
-                    style: TextStyle(
-                      color: CupertinoColors.systemBlue,
+                SizedBox(height: 14),
+                Row(
+                  children: [
+                    SizedBox(width: 14),
+                    Expanded(
+                      child: MainCardItem(
+                        routeName: RouteNames.allCollectionsPage,
+                        iconName: CupertinoIcons.time_solid,
+                        title: AppStrings.quiz,
+                        color: CupertinoColors.systemBlue,
+                      ),
                     ),
-                    textAlign: TextAlign.start,
-                  ),
+                    SizedBox(width: 14),
+                    Expanded(
+                      child: MainCardItem(
+                        routeName: RouteNames.allCollectionsPage,
+                        iconName: CupertinoIcons.table_badge_more_fill,
+                        title: AppStrings.wordsConstructor,
+                        color: CupertinoColors.systemGreen,
+                      ),
+                    ),
+                    SizedBox(width: 14),
+                  ],
                 ),
-                SizedBox(width: 7),
-                CollectionsOrderButton(),
+                SizedBox(height: 14),
+                Row(
+                  children: [
+                    SizedBox(width: 14),
+                    Expanded(
+                      child: MainCardItem(
+                        routeName: RouteNames.allCollectionsPage,
+                        iconName: CupertinoIcons.creditcard_fill,
+                        title: AppStrings.cardMode,
+                        color: CupertinoColors.systemRed,
+                      ),
+                    ),
+                    SizedBox(width: 14),
+                    Expanded(
+                      child: MainCardItem(
+                        routeName: RouteNames.allCollectionsPage,
+                        iconName: CupertinoIcons.collections_solid,
+                        title: AppStrings.allCollections,
+                        color: CupertinoColors.systemIndigo,
+                      ),
+                    ),
+                    SizedBox(width: 14),
+                  ],
+                ),
               ],
             ),
-            const MainCollectionsList(shortCollection: true),
-            CupertinoListSection.insetGrouped(
-              backgroundColor: CupertinoColors.systemBackground,
-              margin: AppStyles.mardingSymmetricHor,
-              decoration: const BoxDecoration(
-                color: CupertinoColors.systemFill,
-                borderRadius: AppStyles.mainBorderMini,
+          ),
+          SliverToBoxAdapter(
+            child: CupertinoListTile(
+              padding: AppStyles.mainMarding,
+              title: Text(
+                AppStrings.lastCollections,
+                style: TextStyle(
+                  fontFamily: 'SF Pro',
+                  fontSize: 22,
+                  letterSpacing: 0.5,
+                  fontWeight: FontWeight.w800,
+                ),
+                textAlign: TextAlign.start,
               ),
-              children: [
-                CupertinoListTile(
-                  onTap: () {
-                    Navigator.pushNamed(context, RouteNames.allCollectionsPage);
-                  },
-                  leading: const Icon(CupertinoIcons.collections),
-                  trailing: const Icon(CupertinoIcons.forward),
-                  title: const Text(AppStrings.allCollections),
-                ),
-                CupertinoListTile(
-                  onTap: () {},
-                  leading: const Icon(CupertinoIcons.time),
-                  trailing: const Icon(CupertinoIcons.forward),
-                  title: const Text(AppStrings.quiz),
-                ),
-                CupertinoListTile(
-                  onTap: () {},
-                  leading: const Icon(CupertinoIcons.table_badge_more),
-                  trailing: const Icon(CupertinoIcons.forward),
-                  title: const Text(AppStrings.wordsConstructor),
-                ),
-                CupertinoListTile(
-                  onTap: () {},
-                  leading: const Icon(CupertinoIcons.creditcard),
-                  trailing: const Icon(CupertinoIcons.forward),
-                  title: const Text(AppStrings.cardMode),
-                ),
-              ],
+              leading: CollectionsOrderButton(),
+              trailing: AddCollectionButton(),
             ),
-            const SizedBox(height: 7),
-            CupertinoListSection.insetGrouped(
-              backgroundColor: CupertinoColors.systemBackground,
-              margin: AppStyles.mardingSymmetricHor,
-              decoration: const BoxDecoration(
-                color: CupertinoColors.systemFill,
-                borderRadius: AppStyles.mainBorderMini,
-              ),
-              children: [
-                CupertinoListTile(
-                  padding: AppStyles.mardingSymmetricHor,
-                  title: const Text(AppStrings.addCollection),
-                  onTap: () {
-                    showCupertinoDialog(
-                      context: context,
-                      builder: (context) {
-                        return const AddCollectionDialog();
-                      },
-                    );
-                  },
-                  trailing: const Icon(CupertinoIcons.add_circled),
-                ),
-              ],
-            ),
-            const SizedBox(height: 7),
-            CupertinoListSection.insetGrouped(
-              backgroundColor: CupertinoColors.systemBackground,
-              margin: AppStyles.mardingSymmetricHor,
-              decoration: const BoxDecoration(
-                color: CupertinoColors.systemFill,
-                borderRadius: AppStyles.mainBorderMini,
-              ),
-              children: [
-                CupertinoListTile(
-                  padding: AppStyles.mardingSymmetricHor,
-                  title: const Text(AppStrings.searchWords),
-                  onTap: () {
-                    showCupertinoModalPopup(
-                      context: context,
-                      builder: (_) => const SearchWordsPage(),
-                    );
-                  },
-                  trailing: const Icon(CupertinoIcons.search),
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+          SliverToBoxAdapter(
+            child: MainCollectionsList(shortCollection: true),
+          ),
+        ],
       ),
     );
   }
