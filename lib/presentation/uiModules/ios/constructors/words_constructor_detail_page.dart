a92e85fae0dd163a85f46882071f5d3a4869695f
dev_muslim <jmappsmuslim@gmail.com>
2024-02-09 10:10:53 +0300

    Added answer counters

--- lib/presentation/uiModules/ios/constructors/words_constructor_detail_page.dart
+++ lib/presentation/uiModules/ios/constructors/words_constructor_detail_page.dart
@@ -8,7 +8,7 @@ import '../../../../data/state/constructor_mode_state.dart';
 import '../../../../data/state/favorite_words_state.dart';
 import '../../../../domain/entities/collection_entity.dart';
 import '../../../../domain/entities/favorite_dictionary_entity.dart';
-import '../widgets/short_translation_text.dart';
+import '../widgets/flip_translation_text.dart';
 
 class WordsConstructorDetailPage extends StatefulWidget {
   const WordsConstructorDetailPage({
@@ -19,12 +19,13 @@ class WordsConstructorDetailPage extends StatefulWidget {
   final CollectionEntity collectionModel;
 
   @override
-  State<WordsConstructorDetailPage> createState() => _WordsConstructorDetailPageState();
+  State<WordsConstructorDetailPage> createState() =>
+      _WordsConstructorDetailPageState();
 }
 
-class _WordsConstructorDetailPageState extends State<WordsConstructorDetailPage> {
+class _WordsConstructorDetailPageState
+    extends State<WordsConstructorDetailPage> {
   final PageController _cardPageController = PageController();
-  List<String> _letters = [];
 
   @override
   Widget build(BuildContext context) {
@@ -35,119 +36,167 @@ class _WordsConstructorDetailPageState extends State<WordsConstructorDetailPage>
         ),
       ],
       child: Consumer<ConstructorModeState>(
-        builder: (BuildContext context, ConstructorModeState constructorModeState, _) {
+        builder: (BuildContext context,
+            ConstructorModeState constructorModeState, _) {
           return CupertinoPageScaffold(
             backgroundColor: CupertinoColors.systemGroupedBackground,
-            child: CustomScrollView(
-              slivers: [
-                CupertinoSliverNavigationBar(
-                  alwaysShowMiddle: false,
-                  middle: Text(widget.collectionModel.title),
-                  largeTitle: const Text('Введите слово'),
-                  previousPageTitle: AppStrings.toBack,
+            navigationBar: CupertinoNavigationBar(
+              middle: Text(widget.collectionModel.title),
+              previousPageTitle: AppStrings.toBack,
+            ),
+            child: SafeArea(
+              bottom: false,
+              child: FutureBuilder(
+                future: Provider.of<FavoriteWordsState>(context)
+                    .fetchFavoriteWordsByCollectionId(
+                  collectionId: widget.collectionModel.id,
                 ),
-                FutureBuilder(
-                  future: Provider.of<FavoriteWordsState>(context).fetchFavoriteWordsByCollectionId(
-                    collectionId: widget.collectionModel.id,
-                  ),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return SliverToBoxAdapter(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            PageView.builder(
-                              controller: _cardPageController,
-                              itemCount: snapshot.data!.length,
-                              itemBuilder: (context, index) {
-                                final FavoriteDictionaryEntity model = snapshot.data![index];
-                                _letters = model.arabicWord.split('');
-                                final List<String> translationLines = model.translation.split('\\n');
-                                return Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    const SizedBox(height: 14),
-                                    Container(
-                                      padding: AppStyles.mainMarding,
-                                      alignment: Alignment.center,
-                                      child: Text(
-                                        constructorModeState.getEndWord,
-                                        style: const TextStyle(
-                                          fontSize: 50,
-                                          fontFamily: 'Uthmanic',
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: PageView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            controller: _cardPageController,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              final FavoriteDictionaryEntity model =
+                                  snapshot.data![index];
+                              final List<String> translationLines =
+                                  model.translation.split('\\n');
+                              return Center(
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(height: 14),
+                                      Container(
+                                        padding: AppStyles.mainMarding,
+                                        alignment: Alignment.center,
+                                        child: Text(
+                                          constructorModeState.getEndWord,
+                                          style: const TextStyle(
+                                            fontSize: 50,
+                                            fontFamily: 'Uthmanic',
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Padding(
-                                      padding: AppStyles.mardingSymmetricHor,
-                                      child: Center(
-                                        child: model.serializableIndex != -1
-                                            ? ShortTranslationText(translation: translationLines[model.serializableIndex])
-                                            : ShortTranslationText(translation: model.translation),
+                                      Padding(
+                                        padding: AppStyles.mardingSymmetricHor,
+                                        child: Center(
+                                          child: model.serializableIndex != -1
+                                              ? FlipTranslationText(
+                                                  translation: translationLines[
+                                                      model.serializableIndex])
+                                              : FlipTranslationText(
+                                                  translation:
+                                                      model.translation),
+                                        ),
                                       ),
-                                    ),
-                                    const SizedBox(height: 14),
-                                    Wrap(
-                                      spacing: 7,
-                                      alignment: WrapAlignment.center,
-                                      runAlignment: WrapAlignment.center,
-                                      children: _letters.map((letter) {
-                                        return CupertinoButton(
-                                          onPressed: constructorModeState.getEndWord.length < model.arabicWord.length ? () => constructorModeState.setEndWord = letter : null,
-                                          child: Text(
-                                            letter,
-                                            style: const TextStyle(
-                                              fontSize: 40,
-                                              fontFamily: 'Uthmanic',
+                                      const SizedBox(height: 14),
+                                      Wrap(
+                                        spacing: 7,
+                                        alignment: WrapAlignment.center,
+                                        runAlignment: WrapAlignment.center,
+                                        children: model.arabicWord
+                                            .split('')
+                                            .map((letter) {
+                                          return CupertinoButton(
+                                            onPressed: constructorModeState
+                                                        .getEndWord.length <
+                                                    model.arabicWord.length
+                                                ? () => constructorModeState
+                                                    .setEndWord = letter
+                                                : null,
+                                            child: Text(
+                                              letter,
+                                              style: const TextStyle(
+                                                fontSize: 40,
+                                                fontFamily: 'Uthmanic',
+                                              ),
+                                              textDirection: TextDirection.rtl,
                                             ),
-                                            textDirection: TextDirection.rtl,
-                                          ),
-                                        );
-                                      }).toList(),
-                                    ),
-                                    const SizedBox(height: 7),
-                                    CupertinoButton(
-                                      onPressed: constructorModeState.getEndWord.length != model.arabicWord.length ? () {
-                                        if (constructorModeState.getEndWord.isNotEmpty) {
-                                          constructorModeState.removeLastLetter();
-                                        }
-                                      } : null,
-                                      child: const Icon(CupertinoIcons.delete_left, size: 30),
-                                    ),
-                                  ],
-                                );
-                              },
-                            ),
-                            const SizedBox(height: 7),
-                            Center(
-                              child: SmoothPageIndicator(
-                                controller: _cardPageController,
-                                count: snapshot.data!.length,
-                                effect: ScrollingDotsEffect(
-                                  maxVisibleDots: 5,
-                                  dotColor: CupertinoColors.systemBlue.withOpacity(0.35),
-                                  activeDotColor: CupertinoColors.systemBlue,
-                                  dotWidth: 10,
-                                  dotHeight: 10,
+                                          );
+                                        }).toList(),
+                                      ),
+                                      const SizedBox(height: 7),
+                                      CupertinoButton(
+                                        onPressed: constructorModeState
+                                                    .getEndWord.length !=
+                                                model.arabicWord.length
+                                            ? () {
+                                                if (constructorModeState
+                                                    .getEndWord.isNotEmpty) {
+                                                  constructorModeState
+                                                      .removeLastLetter();
+                                                }
+                                              }
+                                            : null,
+                                        child: const Icon(
+                                            CupertinoIcons.delete_left,
+                                            size: 30),
+                                      ),
+                                    ],
+                                  ),
                                 ),
+                              );
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 7),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            SizedBox(width: 14),
+                            Text(
+                              '0',
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: CupertinoColors.systemGreen,
+                                fontFamily: 'SF Pro',
                               ),
                             ),
-                            const SizedBox(height: 21),
+                            SizedBox(width: 7),
+                            Text(
+                              '0',
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: CupertinoColors.systemRed,
+                                fontFamily: 'SF Pro',
+                              ),
+                            ),
+                            SizedBox(width: 14),
                           ],
                         ),
-                      );
-                    } else {
-                      return const SliverFillRemaining(
-                        hasScrollBody: false,
-                        child: Center(
-                          child: CupertinoActivityIndicator(),
+                        const SizedBox(height: 7),
+                        Center(
+                          child: SmoothPageIndicator(
+                            controller: _cardPageController,
+                            count: snapshot.data!.length,
+                            effect: ScrollingDotsEffect(
+                              maxVisibleDots: 5,
+                              dotColor:
+                                  CupertinoColors.systemBlue.withOpacity(0.35),
+                              activeDotColor: CupertinoColors.systemBlue,
+                              dotWidth: 10,
+                              dotHeight: 10,
+                            ),
+                          ),
                         ),
-                      );
-                    }
-                  },
-                ),
-              ],
+                        const SizedBox(height: 21),
+                      ],
+                    );
+                  } else {
+                    return const SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Center(
+                        child: CupertinoActivityIndicator(),
+                      ),
+                    );
+                  }
+                },
+              ),
             ),
           );
         },
