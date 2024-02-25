074d3d3fcf45bee5af085bf52758d5f80208eb0a
dev_muslim <jmappsmuslim@gmail.com>
2024-02-25 13:10:33 +0300

    Added app settings page

--- lib/core/routes/material_routes.dart
+++ lib/core/routes/material_routes.dart
@@ -1,22 +1,27 @@
-import 'package:arabic/domain/entities/args/word_move_args.dart';
-import 'package:arabic/presentation/uiModules/android/favorites/move_favorite_word_page.dart';
 import 'package:flutter/material.dart';
 
 import '../../domain/entities/args/collection_args.dart';
 import '../../domain/entities/args/word_args.dart';
 import '../../domain/entities/args/word_favorite_collection_args.dart';
+import '../../domain/entities/args/word_move_args.dart';
 import '../../presentation/uiModules/android/collections/all_collections_page.dart';
 import '../../presentation/uiModules/android/favorites/collection_detail_page.dart';
 import '../../presentation/uiModules/android/favorites/add_favorite_word_page.dart';
 import '../../presentation/uiModules/android/favorites/favorite_word_detail_page.dart';
 import '../../presentation/uiModules/android/favorites/favorite_word_select_collection.dart';
+import '../../presentation/uiModules/android/favorites/move_favorite_word_page.dart';
 import '../../presentation/uiModules/android/search/search_words_page.dart';
 import '../../presentation/uiModules/android/search/word_detail_page.dart';
+import '../../presentation/uiModules/ios/settings/app_settings_page.dart';
 import 'route_names.dart';
 
 class MaterialRoutes {
   static Route onGeneratorRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
+      case RouteNames.appSettingsPage:
+        return MaterialPageRoute(
+          builder: (_) => const AppSettingsPage(),
+        );
       case RouteNames.searchWordsPage:
         return MaterialPageRoute(
           builder: (_) => const SearchWordsPage(),
